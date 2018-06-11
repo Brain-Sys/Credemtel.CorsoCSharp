8d0eeb5b8af6d483a3dafb5bfd6d76207b2275da
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-06-11 16:30:09 +0200

    -

--- Credemtel.CorsoCSharp.Sintassi2/Program.cs
+++ Credemtel.CorsoCSharp.Sintassi2/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,30 @@ namespace Credemtel.CorsoCSharp.Sintassi2
     {
         static void Main(string[] args)
         {
+            Action a1 = new Action(() => { });
+            Action<bool> a2 = new Action<bool>((b) => {
+                if (b == true) writeLog();
+            });
+
+            Func<int, int> f3 = new Func<int, int>((int pippo) => {
+                return pippo * 4;
+            });
+
+            Func<Uri, int, int> f4 = new Func<Uri, int, int>((url, number) => {
+                HttpClient client = new HttpClient();
+                var bytes = client.GetByteArrayAsync(url).Result;
+                return bytes.Length + number;
+            });
+
+            f4.Invoke(new Uri("http://www.google.com"), 890);
+
+            Func<List<DateTime>> x1 = getDates;
+            List<DateTime> date = x1();
+
+            Func<int, int, int, long> funzione = bigNumbers;
+            var y = funzione(53456734, 9000, 46237462);
+
+
             DatabaseLoader loader = new DatabaseLoader();
             loader.DipendeDaOS = new Action(salvataggio);
 
@@ -69,6 +94,11 @@ namespace Credemtel.CorsoCSharp.Sintassi2
             Console.ReadLine();
         }
 
+        private static long bigNumbers(int x, int y, int z)
+        {
+            return x * y * z;
+        }
+
         private static void salvataggio()
         {
             
@@ -104,6 +134,11 @@ namespace Credemtel.CorsoCSharp.Sintassi2
         {
             int v = (int)result.AsyncState;
             bool sync = result.CompletedSynchronously;
+        }
+
+        static List<DateTime> getDates()
+        {
+            return new List<DateTime>();
         }
 
         static void metodoComplesso(bool v1, DateTime v2, List<string> v3, StringBuilder v4)
