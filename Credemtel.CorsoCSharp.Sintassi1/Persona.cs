6745baa47628aa2cd08279b99d35230aa0ccd7b8
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-06-11 10:24:44 +0200

    Add project files.

---
+++ Credemtel.CorsoCSharp.Sintassi1/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credemtel.CorsoCSharp.Sintassi1
+{
+    class Persona
+    {
+    }
+}
---
+++ Credemtel.CorsoCSharp.Sintassi1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credemtel.CorsoCSharp.Sintassi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCollection elencoNomi = new StringCollection();
+            elencoNomi.Add("Liborio");
+            elencoNomi.Add("Mirko");
+            // elencoNomi.Add(90);
+            string x = elencoNomi[0];
+            bool esiste = elencoNomi.Contains("Mirko");
+
+            if(esiste)
+            {
+                int index = elencoNomi.IndexOf("Mirko");
+            }
+
+            BitArray ba = new BitArray(20);
+
+            BitVector32 bv = new BitVector32(56);
+            System.Collections.Specialized.ListDictionary ld = new ListDictionary();
+            ld.Add("dmnlgr", 0);
+            object person = ld["dmnlgr"];
+
+            System.Collections.Stack stack = new Stack();
+            stack.Push(7);
+            stack.Push(DateTime.Now);
+            DateTime dt = (DateTime)stack.Pop();
+
+            ArrayList list = new ArrayList();
+            list.Add(34);
+            list.Add(77.54333);
+            list.Add(true);
+            list.Add(8);
+            list.Add("fnkjgkjdbgkjdf");
+
+            bool obj1 = (bool)list[1];
+            
+            if(obj1 == true)
+            {
+
+            }
+
+            // GENERICS
+            IEnumerable<int> interfaccia;
+            List<int> elencoNumeri = new List<int>();
+            elencoNumeri.Add(7);
+            elencoNumeri.AddRange(new List<int>() { 1, 6, 7 });
+            esiste = elencoNumeri.Contains(6);
+
+            List<double> altezze = new List<double>();
+            List<Persona> partecipanti = new List<Persona>();
+            partecipanti.Add(new Persona());
+
+            Dictionary<string, Persona> anagrafe = new Dictionary<string, Persona>();
+            anagrafe.Add("xyz", new Persona());
+            // anagrafe.Add(555, true);
+        }
+    }
+}
